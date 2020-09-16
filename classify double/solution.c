39f2bca255590ad91770a4835b3bec23b9c002c4
ioiein <pwvaner@mail.ru>
2020-09-16 19:32:06 +0300

    added solution for the first task

--- classify double/solution.c
+++ classify double/solution.c
@@ -16,6 +16,7 @@ uint64_t convertToUint64 (double number) {
 
 bool getBit (const uint64_t number, const uint8_t index) {
     /// Your code here...
+    return ((bool)((number >> index) & 1));
 }
 
 
@@ -25,6 +26,7 @@ bool getBit (const uint64_t number, const uint8_t index) {
 
 bool checkForPlusZero (uint64_t number) {
     /// Your code here.
+    return number == 0;
 }
 
 bool checkForMinusZero (uint64_t number) {
@@ -33,34 +35,66 @@ bool checkForMinusZero (uint64_t number) {
 
 bool checkForPlusInf (uint64_t number) {
     /// Your code here.
+    if (getBit(number, 63) != 0)
+        return false;
+    for (int i = 62; i >= 52; i--)
+    {
+        if (getBit(number, i) != 1)
+            return false;
+    }
+    for (int i = 51; i >= 0; i--)
+    {
+        if (getBit(number, i) != 0)
+            return false;
+    }
+    return true;
 }
 
 bool checkForMinusInf (uint64_t number) {
     /// Your code here.
+    if (getBit(number, 63) != 1)
+        return false;
+    for (int i = 62; i >= 52; i--)
+    {
+        if (getBit(number, i) != 1)
+            return false;
+    }
+    for (int i = 51; i >= 0; i--)
+    {
+        if (getBit(number, i) != 0)
+            return false;
+    }
+    return true;
 }
 
 bool checkForPlusNormal (uint64_t number) {
     /// Your code here.
+    return getBit(number, 62) != getBit(number, 52) && getBit(number, 63) == 0;
 }
 
 bool checkForMinusNormal (uint64_t number) {
     /// Your code here.
+    return getBit(number,62) != getBit(number,52) && getBit(number, 63) == 1;
 }
 
 bool checkForPlusDenormal (uint64_t number) {
     /// Your code here.
+    return getBit(number, 63) == 0 && ((number >> 52) & 0x07FF == 0) && (number & 0x07FFFFFFFFFFFF);
 }
 
 bool checkForMinusDenormal (uint64_t number) {
     /// Your code here.
+    return getBit(number, 63) == 1 && ((number >> 52) & 0x07FF == 0) && (number & 0x07FFFFFFFFFFFF);
 }
 
 bool checkForSignalingNan (uint64_t number) {
     /// Your code here.
+    return getBit(number, 0) != getBit(number, 51) && number >> 52 > 0x07FF;
 }
 
 bool checkForQuietNan (uint64_t number) {
     /// Your code here.
+    return number >> 51 > 0x0FFF;
 }
 
 
